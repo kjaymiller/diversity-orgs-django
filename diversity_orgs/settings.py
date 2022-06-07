406fce3f2a1fe6600dbee9085f2ec10953bada34
Jay Miller <kjaymiller@gmail.com>
2022-06-06 17:59:19 -0700

    remove whitenoise

--- diversity_orgs/settings.py
+++ diversity_orgs/settings.py
@@ -50,8 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-
 ]
 
 ROOT_URLCONF = 'diversity_orgs.urls'
