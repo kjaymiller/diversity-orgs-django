0230b7f77afe27533d7717186dd6afdf95cb63d1
Jay Miller <kjaymiller@gmail.com>
2022-06-08 15:29:59 -0700

    test CORS Policy

--- diversity_orgs/settings.py
+++ diversity_orgs/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'org_pages.apps.OrgPagesConfig', # Orgs
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ORIGIN_WHITELIST = (
+#        'https://diversityorgsstorage.blob.core.windows.net',
+# )
 
 ROOT_URLCONF = 'diversity_orgs.urls'
 
