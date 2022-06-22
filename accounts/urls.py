b560c83bf73564176562e85347de58d0954a88a9
Jay Miller <kjaymiller@gmail.com>
2022-06-22 10:20:02 -0700

    create API Key

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import SignUpView, UpdateUserView
+from .views import CreateAPIKey, SignUpView, UpdateUserView, ResetAPIKey
 
 urlpatterns = [
-    path("signup", SignUpView.as_view(), name="signup"),
-    path("update", UpdateUserView.as_view(), name="update"),
+    path("signup", SignUpView.as_view(), name="account-signup"),
+    path("update", UpdateUserView.as_view(), name="account-update"),
+    path("reset-api-key", ResetAPIKey.as_view(), name="reset-api-key"),
+    path("create-api-key", CreateAPIKey.as_view(), name="create-api-key"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
 from django.urls import reverse_lazy
+from rest_framework.authtoken.models import Token
+from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from django.shortcuts import render, redirect
+import django.contrib.messages
 
 
 class SignUpView(CreateView):
@@ -18,3 +22,41 @@ class UpdateUserView(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['api_key'] = Token.objects.filter(user=self.request.user).first()
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('account-update')
+
+class CreateAPIKey(LoginRequiredMixin, TemplateView):
+    login_url = 'login'
+
+    def get(self, request):
+        """Load the Create API Key page"""
+        return render(request, 'registration/create_api_key.html')
+
+    def post(self, request):
+        """Check if the user already has an API key. If not create one."""
+        if Token.objects.filter(user=request.user):
+            django.contrib.messages.warning(request, 'You already have an API key. You can view it or reset it below.')
+            return redirect('account-update')
+
+        Token.objects.create(user=request.user)
+        django.contrib.messages.success(request, 'API key has been created.')
+        return redirect('account-update')
+
+        
+class ResetAPIKey(LoginRequiredMixin, TemplateView):
+    login_url = 'login'
+
+    def get(self, request):
+        return render(request, 'registration/reset_api_key.html')
+
+    def post(self, request):
+        Token.objects.filter(user=request.user).delete()
+        Token.objects.create(user=request.user)
+        django.contrib.messages.success(request, 'API key has been reset.')
+        return redirect('account-update')
