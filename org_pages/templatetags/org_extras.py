0f1e9c5c5acf8426131887f88c8c9716b4ef24cb
Jay Miller <kjaymiller@gmail.com>
2022-06-23 10:01:40 -0700

    add org_extras

--- org_pages/templatetags/org_extras.py
+++ org_pages/templatetags/org_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from urllib.parse import urlencode
 
 register = template.Library()
 
@@ -6,3 +7,12 @@ register = template.Library()
 @register.filter(name="classname")
 def classname(value):
     return value.__class__.__name__
+
+
+@register.simple_tag
+def urlparams(*_, **kwargs):
+    safe_args = {k: v for k, v in kwargs.items() if v is not None}
+    if safe_args:
+        encoded_args = urlencode(safe_args)
+        return f'?{encoded_args}'
+    return ''
