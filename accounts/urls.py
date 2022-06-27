6c56b3e590ff9e8c7cbe487f1ccd9050e8f7877c
Jay Miller <kjaymiller@gmail.com>
2022-06-27 14:14:19 -0700

    add login url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from .views import CreateAPIKey, SignUpView, UpdateUserView, ResetAPIKey
-
+from .forms import LoginForm
 urlpatterns = [
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="registration/login.html",
+            authentication_form=LoginForm,
+        ),
+        name="login",
+    ),
     path("signup", SignUpView.as_view(), name="account-signup"),
     path("update", UpdateUserView.as_view(), name="account-update"),
     path("reset-api-key", ResetAPIKey.as_view(), name="reset-api-key"),
